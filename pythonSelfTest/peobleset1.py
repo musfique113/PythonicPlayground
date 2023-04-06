f8e4deee4b346a7add05e587fd64451c42a58318
musfiqe113 <musfique113@gmail.com>
2023-04-07 01:12:25 +0600

    py

--- pythonSelfTest/peobleset1.py
+++ pythonSelfTest/peobleset1.py
@@ -9,14 +9,14 @@
 # print('Output B = ',b)
 
 import math
-# x = int(input("Input X: "))
-# y = int(input("Input Y: "))
-# z = int(input("Input Z: "))
-# solve = math.sqrt(y*y-4*x*z)
-# x1 = (-y+solve)/(2*x) 
-# x0 = (-y-solve)/(2*x) 
-# print(x1)
-# print(x0)
+x = int(input("Input X: "))
+y = int(input("Input Y: "))
+z = int(input("Input Z: "))
+solve = math.sqrt(y*y-4*x*z)
+x1 = (-y+solve)/(2*x) 
+x0 = (-y-solve)/(2*x) 
+print(x1)
+print(x0)
 
 
 c = int(input("Input Celcius: "))
