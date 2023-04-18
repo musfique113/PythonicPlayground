585b8cfd966911100d9c1d13b778e4374d79b7c5
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-04-19 00:12:13 +0600

    py

---
+++ pythonSelfTest/practiceproblems/problem07.py
@@ -0,0 +1,9 @@
+'''
+Write a program to print absolute value of a number entered by user. E.g.-
+INPUT: 1        OUTPUT: 1
+INPUT: -1        OUTPUT: 1
+'''
+
+value = int(input('Enter any numbe - '))
+#abs(value)
+print(abs(value))
