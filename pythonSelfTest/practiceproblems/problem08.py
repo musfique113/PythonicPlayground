5df5127504721617b751f79a9857bbbb5b605048
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-04-20 00:34:18 +0600

    problem8,9solved

--- pythonSelfTest/practiceproblems/problem08.py
+++ pythonSelfTest/practiceproblems/problem08.py
@@ -7,13 +7,24 @@ And print
 percentage of class attended
 Is student is allowed to sit in exam or not.
 """
-numberOfClasses = int(input('How many total have been taken:  '))
+numberOfClasses = int(input('How many total have been taken: '))
 numberOfAttendend = int(input("Student attended class: "))
 percentage = (numberOfAttendend * 100) / numberOfClasses
-print(percentage,"% attendend")
-if percentage < 75 :
-    print("Not allow to sit for exam")
+print(percentage,"% attended")
+
+ask = input("Do you have any medical condition? ")
+
+if ask == 'y' or ask == 'Y':
+    print("You can sit for exam with special permission")
+elif ask == 'n' or ask == 'N':
+    if percentage < 75:
+        print("Not allowed to sit for exam")
+    else:
+        print("You can sit for exam")
 elif percentage > 100:
-    print("Parcentage cant be above 100%")
+    print("Percentage can't be above 100%")
 else:
-    print("You can sit for exam")
+    print("Invalid input. Please enter 'y' or 'n'.")
+
+
+
